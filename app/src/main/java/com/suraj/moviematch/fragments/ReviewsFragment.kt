29b42507e8dfa09c56f0114d91e47816a4328379
suraj <surajmargale777@gmail.com>
2023-08-16 13:26:19 +0530

    Ui

--- app/src/main/java/com/suraj/moviematch/fragments/ReviewsFragment.kt
+++ app/src/main/java/com/suraj/moviematch/fragments/ReviewsFragment.kt
@@ -8,16 +8,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
-import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.firebase.auth.FirebaseAuth
-import com.suraj.moviematch.R
 import com.suraj.moviematch.adapter.ReviewAdapter
 import com.suraj.moviematch.app.MyApplication
 import com.suraj.moviematch.dataClasses.MovieReview
